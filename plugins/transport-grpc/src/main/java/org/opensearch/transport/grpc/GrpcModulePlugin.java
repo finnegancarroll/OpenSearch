af8212e84daf2988ea6743212be47e2cce9064e4
Finn Carroll <carrofin@amazon.com>
2024-12-12 10:20:31 -0800

    Min example of 3rd party audit task file copy collision
    Signed-off-by: Finn Carroll <carrofin@amazon.com>

---
+++ plugins/transport-grpc/src/main/java/org/opensearch/transport/grpc/GrpcModulePlugin.java
@@ -0,0 +1,18 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+package org.opensearch.transport.grpc;
+
+import org.opensearch.plugins.NetworkPlugin;
+import org.opensearch.plugins.Plugin;
+
+/**
+ * Main class for the gRPC plugin
+ */
+public final class GrpcModulePlugin extends Plugin implements NetworkPlugin {
+
+}
