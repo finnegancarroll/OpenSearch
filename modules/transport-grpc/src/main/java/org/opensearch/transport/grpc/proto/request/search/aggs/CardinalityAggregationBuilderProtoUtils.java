3e29042f775db75840f1f9de2b37c655ad309792
Finn Carroll <carrofin@amazon.com>
2025-09-29 16:09:55 -0700

    Add cardinality agg name and meta.
    Signed-off-by: Finn Carroll <carrofin@amazon.com>

--- modules/transport-grpc/src/main/java/org/opensearch/transport/grpc/proto/request/search/aggs/CardinalityAggregationBuilderProtoUtils.java
+++ modules/transport-grpc/src/main/java/org/opensearch/transport/grpc/proto/request/search/aggs/CardinalityAggregationBuilderProtoUtils.java
@@ -10,8 +10,10 @@ package org.opensearch.transport.grpc.proto.request.search.aggs;
 
 import org.opensearch.protobufs.CardinalityAggregation;
 import org.opensearch.protobufs.FieldValue;
+import org.opensearch.protobufs.ObjectMap;
 import org.opensearch.script.Script;
 import org.opensearch.search.aggregations.metrics.CardinalityAggregationBuilder;
+import org.opensearch.transport.grpc.proto.request.common.ObjectMapProtoUtils;
 import org.opensearch.transport.grpc.proto.request.common.ScriptProtoUtils;
 import org.opensearch.transport.grpc.proto.response.common.FieldValueProtoUtils;
 
@@ -38,14 +40,12 @@ public class CardinalityAggregationBuilderProtoUtils {
      * @throws IOException if there's an error during parsing
      */
     protected static CardinalityAggregationBuilder fromProto(CardinalityAggregation cardinalityAggregation) throws IOException {
-        // TODO: Replace temp name with actual when api spec is updated
-        CardinalityAggregationBuilder builder = new CardinalityAggregationBuilder("_name");
+        CardinalityAggregationBuilder builder = new CardinalityAggregationBuilder(cardinalityAggregation.getName());
 
-        // TODO: Cardinality agg missing object metadata in api spec
-//        if (cardinalityAggregation.hasMeta()) {
-//            ObjectMap objMap = cardinalityAggregation.getMeta();
-//            builder.setMetadata(ObjectMapProtoUtils.fromProto(objMap));
-//        }
+        if (cardinalityAggregation.hasMeta()) {
+            ObjectMap objMap = cardinalityAggregation.getMeta();
+            builder.setMetadata(ObjectMapProtoUtils.fromProto(objMap));
+        }
 
         if (cardinalityAggregation.hasField()) {
             builder.field(cardinalityAggregation.getField());
