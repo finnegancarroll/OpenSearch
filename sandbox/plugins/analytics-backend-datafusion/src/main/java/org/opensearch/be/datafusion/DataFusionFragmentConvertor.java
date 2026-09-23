a12d4f1153abf37e67d6845d1da579fdee02fe0f
Finn Carroll <carrofin@amazon.com>
2026-09-23 11:25:36 -0700

    feat(analytics): map ARRAY_CONTAINS to DataFusion array_has
    The SQL frontend rewrites implicit multi_value =/!= into ARRAY_CONTAINS
    (existential element membership). Map that Calcite operator to DataFusion's
    native array_has, which is registered on the shard layer via
    SessionStateBuilder.with_default_features() (same path as array_min/max/distinct).
    No Rust change required.
    
    Signed-off-by: Finn Carroll <carrofin@amazon.com>

--- sandbox/plugins/analytics-backend-datafusion/src/main/java/org/opensearch/be/datafusion/DataFusionFragmentConvertor.java
+++ sandbox/plugins/analytics-backend-datafusion/src/main/java/org/opensearch/be/datafusion/DataFusionFragmentConvertor.java
@@ -212,6 +212,9 @@ public class DataFusionFragmentConvertor implements FragmentConvertor {
         FunctionMappings.s(SqlLibraryOperators.ARRAY_LENGTH, "array_length"),
         FunctionMappings.s(SqlLibraryOperators.ARRAY_SLICE, "array_slice"),
         FunctionMappings.s(SqlLibraryOperators.ARRAY_DISTINCT, "array_distinct"),
+        // Implicit multi_value =/!= membership from the frontend emits ARRAY_CONTAINS; DataFusion's
+        // native array-membership function is array_has (registered via with_default_features).
+        FunctionMappings.s(SqlLibraryOperators.ARRAY_CONTAINS, "array_has"),
         FunctionMappings.s(MakeArrayAdapter.LOCAL_MAKE_ARRAY_OP, "make_array"),
         FunctionMappings.s(ArrayToStringAdapter.LOCAL_ARRAY_TO_STRING_OP, "array_to_string"),
         FunctionMappings.s(ArrayElementAdapter.LOCAL_ARRAY_ELEMENT_OP, "array_element"),
